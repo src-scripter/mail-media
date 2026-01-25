d4a6e940e14c6d51135c44d74f37995a37513deb
src-scripter <zach83f@proton.me>
2026-01-24 16:16:52 -0800

    Update index.js
    Updated comments, logs, and main class. Also added the retrieval part of the process with ImapFlow.

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const fs = require('fs');
-const imapflow = require('imapflow');
+const { ImapFlow } = require('imapflow');
 const nodemailer = require('nodemailer');
 const openpgp = require('openpgp');
-const postal_mime = require('postal-mime');
+// const postal_mime = require('postal-mime');
+// ^ For additional parsing.
+console.log('This is a proof of concept for the mail media proposal.');
 
 // Step #1: Gather Information
 console.log('Gathering information...');
+// In production, implement a more secure way of storing credentials.
 const full_name = 'John Doe';
 const email_username = 'your_email@example.com';
 const email_password = 'your_email_password@example.com';
-const pgp_password = 'thisisatest'; // For proof of concept.
+const pgp_password = 'thisisatest';
 
 // Step #2: Generate Keypair
 console.log('Generating keypair...');
-// RSA for simplicity. Change cipher on final product!!!
+// RSA for simplicity. Change cipher on final product!
 (async () => {
     const { privateKey, publicKey } = await openpgp.generateKey({
         type: 'rsa',
@@ -23,6 +26,7 @@ console.log('Generating keypair...');
     });
 })();
 
+// There are a lot more email providers out there. Make sure they support IMAP & SMTP connections.
 const email_providers = {
 	'gmail.com': 'smtp.gmail.com',
 	'outlook.com': 'smtp-mail.outlook.com',
@@ -35,30 +39,33 @@ const email_providers = {
 // Step #3: Create Data for Storage & Usage
 console.log('Creating data for storage & usage...');
 class email_settings {
-	constructor () {
-		let provider = email_username.trim().split('@')[1].toLowerCase();
+	constructor (user_address, user_password, pubKey, priKey) {
+		let provider = user_address.trim().split('@')[1].toLowerCase();
 		if (!provider in email_providers) {
 			throw new Error('Unsupported email address provided.');
 		} else {
 			this.host = email_providers['provider'];
 		}
-		this.port = 587;
+		this.SMTPport = 587;
+		this.IMAPport = 993;
 		this.secure = true;
-		this.auth_user = email_username;
-		this.auth_password = email_password;
-		this.public_key = publicKey;
-		this.private_key = privateKey;
+		this.auth_user = user_address;
+		this.auth_password = user_password;
+		this.public_key = pubKey;
+		this.private_key = priKey;
 	}
 }
-const new_user = new email_settings();
+const new_user = new email_settings(email_username, email_password, publicKey, privateKey);
 console.log(`User Information: ${new_user}`);
-
 // By this point, you'd choose where to store this newly generated user data.
+localStorage.setItem('testuser', new_user);
+
 // Step #4: Retrieve Data & Send Encrypted Test Email
+localStorage.getItem('testuser');
 console.log('Sending test email...')
 const transporter = nodemailer.createTransport({
   host: new_user.host,
-  port: new_user.port,
+  port: new_user.SMTPport,
   secure: new_user.secure,
   auth: {
     user: new_user.auth_user,
@@ -67,7 +74,7 @@ const transporter = nodemailer.createTransport({
 });
 
 // Get Template Email
-fs.readFile('./email_template.html', 'utf8', (err, data) => {
+const email = fs.readFile('./email_template.html', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
@@ -79,13 +86,47 @@ fs.readFile('./email_template.html', 'utf8', (err, data) => {
   const message = await transporter.sendMail({
     from: '"nodejsmailmedia@example.com"',
     to: new_user.auth_user,
-    subject: "NodeJS Mail Media Test Message",
-    html: "",
+    subject: 'NodeJS Mail Media Test Message',
+    html: email,
   });
   console.log("Message sent:", message.messageId);
 })();
 
 // Step #5: Ensure Email Received + Data Parsable
-console.log('Checking inbox...');
-// Will need to add a waiter while mailbox receives message
-console.log('Please wait while the program looks for and receives the test message.');
+const emailClient = new ImapFlow({
+    host: new_user.host,
+    port: new_user.IMAPport,
+    secure: new_user.secure,
+    auth: {
+        user: new_user.auth_user,
+        pass: new_user.auth_password
+    },
+    logger: false
+});
+
+async function inboxSession() {
+	console.log('Checking inbox...');
+	console.log('Please wait while the program looks for and receives the test message.');    
+	await emailClient.connect();
+    console.log('Connected!');
+
+	let lock = await emailClient.getMailboxLock('INBOX');
+	try {
+		let searchedSubject = await emailClient.search({ subject: 'NodeJS Mail Media Test Message' }, { uid: true })
+    	console.log('Email found!');
+		console.log('Attempting retrieval...');
+		let retrievedMessage = await emailClient.fetchOne(searchedSubject, { envelope: true, bodyStructure: true}, { uid: true });
+		console.log('Email retrieved!');
+		console.log(retrievedMessage.envelope);
+	} catch (err) {
+		console.log('Search & retrieval failed!');
+		console.log(err);
+		return;
+	} finally {
+  	  lock.release();
+	}
+}
+
+inboxSession().catch(err => {
+    console.error('Connection error:', err);
+});
